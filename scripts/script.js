93743026ea03b7efefdbd3152318790a219ab19a
prodis-opus19 <101759291+prodis-opus19@users.noreply.github.com>
2022-08-02 12:42:51 +0200

    cleanup js

--- scripts/script.js
+++ scripts/script.js
@@ -2,33 +2,29 @@ function close_hamburger_menu() {
     document.getElementById("checkbox_toggle").checked = false;
 }
 
-function open_tab(pageName, elmnt) {
+
+function open_tab(page_name, html_tag) {
     // close mobile hamburger menu on tab select
     close_hamburger_menu();
-    // hide all elements with class="full_page_tab" by default */
+    // hide all elements with class="full_page_tab" by default
     var i, full_page_tab, navbar_links;
     full_page_tab = document.getElementsByClassName("full_page_tab");
     for (i = 0; i < full_page_tab.length; i++) {
         full_page_tab[i].style.display = "none";
     }
-
     // remove the background color of all navbar_links/buttons
     navbar_links = document.getElementsByClassName("navbar_link");
     for (i = 0; i < navbar_links.length; i++) {
         navbar_links[i].style.backgroundColor = "";
     }
-
     // show the specific tab content
-    document.getElementById(pageName).style.display = "block";
-
+    document.getElementById(page_name).style.display = "block";
     // set active tab color to red
-    elmnt.style.backgroundColor = "#bb4b4b";
-
+    html_tag.style.backgroundColor = "#bb4b4b";
     // scroll to top
     document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
     document.body.scrollTop = 0; // for Safari
 }
-
 
 
 function open_person_desc(evt, person_name) {
@@ -48,10 +44,9 @@ function open_person_desc(evt, person_name) {
     evt.currentTarget.className += " active";
 }
 
+
 // on script load...
 // get the element with id="default_full_page_tab_open" and click on it
 document.getElementById("default_full_page_tab_open").click();
 // get the element with id="default_per_person_tab_open" and click on it
 document.getElementById("default_per_person_tab_open").click();
-
-
