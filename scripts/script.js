fbdcbd4b5bbcf6142d461b405a96d01cdc52b75a
prodis-opus19 <101759291+prodis-opus19@users.noreply.github.com>
2022-08-14 17:17:56 +0200

    set main page lang to polish if redirected from recruitment page

--- scripts/script.js
+++ scripts/script.js
@@ -44,21 +44,31 @@ function open_person_desc(evt, person_name) {
     evt.currentTarget.className += " active";
 }
 
-function switch_language(img_class) {
+function switch_language() {
     // Toggle between Polish and English
     if (document.body.className === "hide_polish") {
         document.body.className = "hide_english"; // hide tags with "en" ID
-        document.getElementById(img_class).src = "img/flag_poland.png"; // set polish flag
+        document.getElementById('lang_flag').src = "img/flag_poland.png"; // set polish flag
     }
     else {
         document.body.className = "hide_polish"; // hide tags with "pl" ID
-        document.getElementById(img_class).src = "img/flag_us.png"; // set american flag
+        document.getElementById('lang_flag').src = "img/flag_us.png"; // set american flag
     }
 }
-
 
 // on script load...
 // get the element with id="default_full_page_tab_open" and click on it
 document.getElementById("default_full_page_tab_open").click();
 // get the element with id="default_per_person_tab_open" and click on it
 document.getElementById("default_per_person_tab_open").click();
+// if set language parameter (en, pl) passed, set language
+// file:///Users/hikari/Work/prodis-opus19.github.io/index.html?lang=pl
+const param_lang = new URLSearchParams(location.search).get("lang");
+if (param_lang === "pl") {
+    document.body.className = "hide_english"; // hide tags with "en" ID
+    document.getElementById('lang_flag').src = "img/flag_poland.png"; // set polish flag
+}
+else if (param_lang === "en") {
+    document.body.className = "hide_polish"; // hide tags with "pl" ID
+    document.getElementById('lang_flag').src = "img/flag_us.png"; // set american flag
+}
