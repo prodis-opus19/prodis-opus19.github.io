861f2935c53bdfd960229040f4ac01557ebc0801
prodis-opus19 <101759291+prodis-opus19@users.noreply.github.com>
2022-08-15 20:35:24 +0200

    cleanup js logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,53 @@
+function capitalize_first_letter(string) {
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+
 function close_hamburger_menu() {
     document.getElementById("checkbox_toggle").checked = false;
 }
 
 
-function open_tab(page_name, html_tag) {
+function open_tab(category) {
     // close mobile hamburger menu on tab select
     close_hamburger_menu();
     // hide all elements with class="full_page_tab" by default
-    var i, full_page_tab, navbar_links;
-    full_page_tab = document.getElementsByClassName("full_page_tab");
-    for (i = 0; i < full_page_tab.length; i++) {
+    const full_page_tab = document.getElementsByClassName("full_page_tab");
+    for (let i = 0; i < full_page_tab.length; i++) {
         full_page_tab[i].style.display = "none";
     }
     // remove the background color of all navbar_links/buttons
-    navbar_links = document.getElementsByClassName("navbar_link");
-    for (i = 0; i < navbar_links.length; i++) {
+    const navbar_links = document.getElementsByClassName("navbar_link");
+    for (let i = 0; i < navbar_links.length; i++) {
         navbar_links[i].style.backgroundColor = "";
     }
     // show the specific tab content
-    document.getElementById(page_name).style.display = "block";
-    // set active tab color to red
-    html_tag.style.backgroundColor = "#bb4b4b";
+    document.getElementById(`${category}_tab`).style.display = "block";
+    // set active tab's button color to red
+    document.getElementById(`${category}_button`).style.backgroundColor = "#bb4b4b";
+    // set webpage's title
+    // document.title = `PRODIS - ${capitalize_first_letter(category)}`;
     // scroll to top
     document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
     document.body.scrollTop = 0; // for Safari
 }
 
 
-function open_person_desc(evt, person_name) {
-    var i, tab_per_person_content, tab_per_person_link;
+function open_person_desc(category) {
     // get all elements with class="tab_per_person_content" and hide them
-    tab_per_person_content = document.getElementsByClassName("tab_per_person_content");
-    for (i = 0; i < tab_per_person_content.length; i++) {
+    const tab_per_person_content = document.getElementsByClassName("tab_per_person_content");
+    for (let i = 0; i < tab_per_person_content.length; i++) {
         tab_per_person_content[i].style.display = "none";
     }
     // get all elements with class="tab_per_person_link" and remove the class "active"
-    tab_per_person_link = document.getElementsByClassName("tab_per_person_link");
-    for (i = 0; i < tab_per_person_link.length; i++) {
+    const tab_per_person_link = document.getElementsByClassName("tab_per_person_link");
+    for (let i = 0; i < tab_per_person_link.length; i++) {
         tab_per_person_link[i].className = tab_per_person_link[i].className.replace(" active", "");
     }
-    // show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(person_name).style.display = "block";
-    evt.currentTarget.className += " active";
+    // show the current tab
+    document.getElementById(`${category}_tab`).style.display = "block";
+    // add an "active" class to the button that opened the tab
+    document.getElementById(`${category}_button`).className += " active";
 }
 
 function switch_language() {
@@ -58,9 +64,9 @@ function switch_language() {
 
 // on script load...
 // get the element with id="default_full_page_tab_open" and click on it
-document.getElementById("default_full_page_tab_open").click();
+document.getElementById("project_button").click();
 // get the element with id="default_per_person_tab_open" and click on it
-document.getElementById("default_per_person_tab_open").click();
+document.getElementById("m_kul_button").click();
 // if set language parameter (en, pl) passed, set language
 // file:///Users/hikari/Work/prodis-opus19.github.io/index.html?lang=pl
 const param_lang = new URLSearchParams(location.search).get("lang");
