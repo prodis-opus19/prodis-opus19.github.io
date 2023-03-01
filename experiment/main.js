721fab5ab23b9e8a4c1538fb3ee5f64896ae3a93
prodis-opus19 <101759291+prodis-opus19@users.noreply.github.com>
2023-03-01 23:30:39 +0100

    add improved text selection with indicator

--- experiment/main.js
+++ experiment/main.js
@@ -1,21 +1,99 @@
+import { get_previous_letter, get_next_letter } from "/scripts/letter_loop.js";
+
 // GLOBAL VARIABLES
-const CONTAINER_GROUPS = document.getElementsByClassName("container_group");
-const SELECTED_GROUP_INDICATOR = document.getElementById("group_select_indicator");
+const GROUP_DROPDOWN = document.getElementById("group_dropdown");
+const GROUP_CONTAINERS = document.getElementsByClassName("group_container");
+
+const TEXT_DROPDOWN = document.getElementById("text_dropdown");
+const TEXT_CONTAINERS = document.getElementsByClassName("text_container");
+
+const SELECTION_INDICATOR = document.getElementById("selection_indicator");
+
+let CURRENT_GROUP_NUMBER = "1"; // changed using open_group();
+let CURRENT_TEXT_LETTER = "A"; // changed using open_text();
 
 
-function open_group(name) {
-    // set indicator to last letter (e.g., "group3" -> "3")
-    SELECTED_GROUP_INDICATOR.textContent = name.slice(-1);
-    // hide all groups
-    for (const div of CONTAINER_GROUPS) {
+function scroll_up_to_text() {
+    /*
+    Scroll to the beginning of the text.
+    Called when switching between different texts.
+    */
+    window.scrollTo({ top: 500, behavior: 'smooth' });
+}
+
+function scroll_to_top() {
+    /*
+    Scroll to the top of the page.
+    */
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+
+function set_indicator() {
+    /*
+    Set indicator at the bottom to group number and letter, e.g., 1A, 2D, 3C.
+    */
+    SELECTION_INDICATOR.textContent = CURRENT_GROUP_NUMBER + CURRENT_TEXT_LETTER;
+}
+
+
+function open_group(number) {
+    // close dropdown menu (if doesn't exist, no error)
+    GROUP_DROPDOWN.removeAttribute("open");
+    // hide all groups (1, 2, 3)
+    for (const div of GROUP_CONTAINERS) {
         div.style.display = "none";
     }
     // show the specific group content
-    document.getElementById(`article_${name}`).style.display = "block";
+    document.getElementById(`container_group${number}`).style.display = "block";
+    // set global variable
+    CURRENT_GROUP_NUMBER = number;
+    // open text A by default
+    open_text("A");
+}
+
+function open_text(letter) {
+    // close dropdown menu (if doesn't exist, no error)
+    TEXT_DROPDOWN.removeAttribute("open");
+    // hide all texts (1A, 1B, 1C, ..., 2B, 2C, 2D, ..., 3B, 3D, etc.)
+    for (const text of TEXT_CONTAINERS) {
+        text.style.display = "none";
+    }
+    // show the specific article content
+    document.getElementById(`${CURRENT_GROUP_NUMBER}${letter}`).style.display = "block";
+    // set global variable
+    CURRENT_TEXT_LETTER = letter;
+    // set indicator at the bottom to current group & text
+    set_indicator();
+}
+
+function previous_text() {
+    /*
+    Open previous text in order, e.g., B -> A.
+    Loop back from D back to A.
+    */
+    const previous_letter = get_previous_letter(CURRENT_TEXT_LETTER);
+    open_text(previous_letter);
+    scroll_up_to_text();
+}
+
+function next_text() {
+    /*
+    Open next text in order, e.g., A -> B.
+    Loop back from A back to D.
+    */
+    const next_letter = get_next_letter(CURRENT_TEXT_LETTER);
+    open_text(next_letter);
+    scroll_up_to_text();
 }
 
 // select first group by default
-open_group("group1");
+open_group("1");
+open_text("A");
 
 // allow global access within HTML
 window.open_group = open_group;
+window.open_text = open_text;
+window.previous_text = previous_text;
+window.next_text = next_text;
+window.scroll_to_top = scroll_to_top;
