a394a0f8c7e22a219ed93ca2c50654f1359bd1f9
prodis-opus19 <101759291+prodis-opus19@users.noreply.github.com>
2023-03-15 18:47:03 +0100

    add language switching for buttons

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ import { open_tab } from "/scripts/tab.js";
 // bool check for ancient browsers
 const local_storage_available = (typeof (Storage) !== "undefined");
 
+const TAB_LINKS = document.getElementsByClassName("tab_link");
+
 
 function open_person_desc(category = null) {
     /*
@@ -32,6 +34,39 @@ function open_person_desc(category = null) {
     document.getElementById(`${category}_button`).classList.add("active");
 }
 
+function set_nav_btns(translate_to_polish = false) {
+    /*
+    Translate buttons to Polish or English.
+    This is pretty ugly, but I can't think of a better way while using IDs for
+        buttons; I designed them with a single language in mind.
+    */
+    const btn_names_en = new Map([
+        ["project_button", "Project"],
+        ["team_button", "Team"],
+        ["contact_button", "Contact"],
+        ["recordings_button", "Recordings"],
+        ["lang_button", "Language"],
+    ]);
+    const btn_names_pl = new Map([
+        ["project_button", "Projekt"],
+        ["team_button", "Skład"],
+        ["contact_button", "Kontakt"],
+        ["recordings_button", "Nagrania"],
+        ["lang_button", "Język"],
+    ]);
+    for (const btn of TAB_LINKS) {
+        if (!btn.id) { // no ID tag
+            continue;
+        }
+        const actual_text = btn.children[1]; // get 2nd item (<span>)
+        if (!actual_text) { // if undefined, someone broke the html
+            console.error(`Skipping button with empty text: '${actual_text}'. The HTML is probably broken.`);
+            continue;
+        }
+        actual_text.textContent = translate_to_polish ? btn_names_pl.get(btn.id) : btn_names_en.get(btn.id);
+    }
+}
+
 
 function set_english(show_alert = true) {
     if (show_alert) {
@@ -39,6 +74,7 @@ function set_english(show_alert = true) {
     }
     document.body.className = "hide_polish"; // hide tags with lang="pl" ID
     document.getElementById("lang_flag").src = "images/root/flag/us.png"; // set american flag src
+    set_nav_btns(false);
     document.documentElement.setAttribute("lang", "en"); //
     if (local_storage_available) { // store language in local storage (5MB)
         window.localStorage.setItem("lang", "en");
@@ -52,6 +88,7 @@ function set_polish(show_alert = true) {
     }
     document.body.className = "hide_english"; // hide tags with lang="en" ID
     document.getElementById("lang_flag").src = "images/root/flag/pl.png"; // set polish flag src
+    set_nav_btns(true);
     document.documentElement.setAttribute("lang", "pl"); // html lang tag
     if (local_storage_available) { // store language in local storage (5MB)
         window.localStorage.setItem("lang", "pl");
@@ -70,7 +107,7 @@ function toggle_language() {
 }
 
 
-function set_language() {
+function set_language_on_page_load() {
     /*
     Set Polish based on local storage and browser's language, but prioritize local storage.
     */
@@ -91,7 +128,7 @@ function set_language() {
 
 // on page load
 open_person_desc();
-set_language();
+set_language_on_page_load();
 
 
 // allow global access within HTML
