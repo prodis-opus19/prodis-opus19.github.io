f1fb3c60a2fad729dafd92e1304a070a31be5ab6
prodis-opus19 <101759291+prodis-opus19@users.noreply.github.com>
2023-03-28 19:38:12 +0200

    add simple word count, tweak css

--- tools/main.js
+++ tools/main.js
@@ -1,3 +1,18 @@
+document.getElementById("simple_count_words_btn").addEventListener("click", function () {
+    let raw_str = document.getElementById('simple_count_words').value;
+    let count = 0;
+    // keep at 0 on empty string
+    if (!raw_str) {
+        console.warn("No string provided.");
+    } else {
+        raw_str = raw_str.toLowerCase(); // turn lowercase
+        const words_split = raw_str.split(' '); // split at newlines
+        count = words_split.length;
+    }
+    document.getElementById("simple_word_count_output").textContent = count;
+});
+
+
 document.getElementById("uniq_words_btn").addEventListener("click", function () {
     let raw_str = document.getElementById('uniq_words').value;
     let count_unique = 0, count_total = 0, count_ratio = 0;
