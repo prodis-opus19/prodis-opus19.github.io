b70e6eafff7d5247e0da6d9a18e6d0e447e8819b
prodis-opus19 <101759291+prodis-opus19@users.noreply.github.com>
2023-04-05 17:08:58 +0200

    merge letter loop

--- experiment/reading/main.js
+++ experiment/reading/main.js
@@ -19,6 +19,45 @@ const SCROLL_TARGET = document.getElementById("scroll_target");
 let CURRENT_GROUP_NUMBER = "1"; // changed using open_group();
 let CURRENT_TEXT_LETTER = "A"; // changed using open_text();
 
+function get_next_letter(letter) {
+    /*
+    Get letter that comes after alphabetically till "D", then loop back to "A".
+    Ugly, but simple and error-proof.
+    */
+    switch (letter) {
+        case "A":
+            return "B";
+        case "B":
+            return "C";
+        case "C":
+            return "D";
+        case "D":
+            return "A";
+        default:
+            console.error(`Letter provided ${letter} is not 'A', 'B', 'C' or 'D', using 'A' as fallback.`)
+            return "A";
+    }
+}
+
+function get_previous_letter(letter) {
+    /*
+    Get letter that comes before alphabetically till "A", then loop back to "D".
+    Ugly, but simple and error-proof.
+    */
+    switch (letter) {
+        case "D":
+            return "C";
+        case "C":
+            return "B";
+        case "B":
+            return "A";
+        case "A":
+            return "D";
+        default:
+            console.error(`Letter provided ${letter} is not 'A', 'B', 'C' or 'D', using 'A' as fallback.`)
+            return "A";
+    }
+}
 
 function scroll_up_to_text() {
     /*
